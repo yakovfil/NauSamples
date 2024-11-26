92f7743fb810dbfeda3cb8143f6d87d3f23f4453
Andrey Karsakov <kapc3d@gmail.com>
2024-11-26 11:09:58 +0300

    fix-source-compilation

--- inputDemo/input_control_component.h
+++ inputDemo/input_control_component.h
@@ -1,6 +1,6 @@
 // Copyright 2024 N-GINN LLC. All rights reserved.
 // Use of this source code is governed by a BSD-3 Clause license that can be found in the LICENSE file.
-﻿
+
 #pragma once
 #include "nau/input_system.h"
 #include "nau/scene/components/component_life_cycle.h"
--- inputDemo/input_test.h
+++ inputDemo/input_test.h
@@ -1,6 +1,6 @@
 // Copyright 2024 N-GINN LLC. All rights reserved.
 // Use of this source code is governed by a BSD-3 Clause license that can be found in the LICENSE file.
-﻿
+
 #pragma once
 #include <EASTL/string.h>
 
